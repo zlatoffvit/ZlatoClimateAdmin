e9d02453d31694d4c70f8c3d3652aeb13741573c
Vitalii Zlatov <zlatov.vit@gmail.com>
2025-06-06 19:52:20 +0100

    Updating middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,10 @@
 import { auth } from "@/auth"
-import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
+import { 
+  apiAuthPrefix, 
+  authRoutes, 
+  DEFAULT_LOGIN_REDIRECT, 
+  publicRoutes 
+} from "./routes";
  
 export default auth((req) => {
   const { nextUrl } = req;
@@ -10,14 +15,14 @@ export default auth((req) => {
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return null;
+    return;
   }
 
   if (isAuthRoute) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
-    return null;
+    return;
   }
 
   if (!isLoggedIn && !isPublicRoute) {
@@ -34,7 +39,7 @@ export default auth((req) => {
     ));
   }
 
-  return null;
+  return;
 })
 
 
